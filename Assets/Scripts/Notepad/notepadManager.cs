4fea4deed0fedf70e10d5fff1a2e9f8af57a8c13
Tomm9000 <tmulbussiness@gmail.com>
2025-10-22 14:40:02 +0200

    Chore[NotePad] Cleans up some code and commenting

--- Assets/Scripts/Notepad/notepadManager.cs
+++ Assets/Scripts/Notepad/notepadManager.cs
@@ -1,29 +1,28 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
-namespace UI
+namespace Notepad
 {
     public class NotepadManager : MonoBehaviour
     {
-        [SerializeField] private GameObject notepadUI;   // The notepad panel op de rechterkant van het scherm
-        [SerializeField] private TMP_InputField inputField; // The text input field voor notes
+        [SerializeField] private GameObject notepadUI;                      // The notepad panel op de rechterkant van het scherm
+        [SerializeField] private TMP_InputField inputField;                 // The text input field voor notes
+        [SerializeField] private KeyCode openNotePad = KeyCode.UpArrow;     // The button needed to open the notepad
 
-        private bool _isOpen = false; // Let op of notepad open is
+        private bool _isOpen;                                               // Let op of notepad open is
 
-        private void Start() => notepadUI.SetActive(false);
+        private void Start() => 
+            notepadUI.SetActive(false);
         
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.UpArrow)) // checked voor input
-                ToggleNotepad(); // Toggled Notepad open/dicht
+        private void Update() =>
+            ToggleNotepad();
 
-            if (_isOpen && Input.GetKeyDown(KeyCode.Escape))
-                ToggleNotepad();
-        }
-
+        /// <summary>
+        /// Toggles the Notepad on and off
+        /// </summary>
         private void ToggleNotepad()
         {
-            // logica voor open en dicht doen
+            if (!Input.GetKeyDown(openNotePad)) return;
             _isOpen = !_isOpen;
             notepadUI.SetActive(_isOpen);
 
@@ -33,13 +32,18 @@ namespace UI
                 CloseNotepad();
         }
 
+        /// <summary>
+        /// opens the notepad by activating the ui asset and activates the ability to select and type in it
+        /// </summary>
         private void OpenNotepad()
         {
-            //Activate de input van de toetsen
             inputField.ActivateInputField();
             inputField.Select();
         }
 
+        /// <summary>
+        /// Closes the notepad by deactivating the ui asset
+        /// </summary>
         private void CloseNotepad() => inputField.DeactivateInputField();
         
     }
